3518ca0460dd43f8e76e0dc700172f3957b91836
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-04-02 23:01:49 +0900

    Grep 6

---
+++ grep_6/src/main.rs
@@ -0,0 +1,34 @@
+use rayon::prelude::*;
+use std::fs::read_to_string;
+use structopt::StructOpt;
+
+#[derive(StructOpt)] // => 「StructOptとレイトを継承する」という意味
+#[structopt(name = "rsgrep")]
+struct GrepArgs {
+    #[structopt(name = "PATTERN")]
+    pattern: String,
+    #[structopt(name = "FILE")]
+    path: Vec<String>,
+}
+
+fn grep(content: &str, pattern: &str, path: &str) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            println!("{}: {}", path, line);
+        }
+    }
+}
+
+fn run(state: GrepArgs) {
+    state
+        .path
+        .par_iter()
+        .for_each(|file| match read_to_string(file) {
+            Ok(content) => grep(&content, &state.pattern, file),
+            Err(reason) => println!("{}", reason),
+        });
+}
+
+fn main() {
+    run(GrepArgs::from_args());
+}
